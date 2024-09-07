8f0e9d81b99aa36d4d573b4f55060cdd09841f99
ShaikFaheemuddin789 <shaikfaheemudin789@gmail.com>
2024-09-07 10:48:27 +0530

    added emojis for raange as well

--- price-range-adjuster/src/components/RangeAdjuster.jsx
+++ price-range-adjuster/src/components/RangeAdjuster.jsx
@@ -1,17 +1,25 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const RangeAdjuster = () => {
-  const [range, setRange] = useState(50);
-
+  const [range, setRange] = useState(0);
+  const [emoji, setEmoji] = useState("🥲");
   function handleChange(e) {
     setRange(e.target.value);
   }
+  useEffect(() => {
+    if (range == 10) {
+      setEmoji("🥰");
+    } else if (range == 20) setEmoji("👍");
+    else if (range >= 30 && range <= 40) setEmoji("🤩");
+    else if (range >= 50) setEmoji("❤️");
+  }, [range]);
 
   return (
     <div>
+      <h1>{emoji}</h1>
       <input
         type="range"
-        min="10"
+        min="0"
         max="1000"
         step="10"
         onChange={handleChange}
